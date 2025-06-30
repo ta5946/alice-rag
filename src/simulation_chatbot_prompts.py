020b2d4da4d0c6877e4ccce02345df864eae936e
ta5946 <ta5946@student.uni-lj.si>
2025-06-30 10:31:16 +0200

    Prompt tuning

--- src/simulation_chatbot_prompts.py
+++ src/simulation_chatbot_prompts.py
@@ -2,25 +2,27 @@ from langchain_core.messages import SystemMessage
 
 
 default_message_history = [
-    SystemMessage(content="There is no conversation history yet.")
+    SystemMessage(content="This is the start of a new conversation. There is no message history yet.")
 ]
 
 classifier_system_message = SystemMessage(
-    content="""You are a prompt classifier.
-    Your task is to classify the following prompt into one of the categories:
-    1. General question, that can be answered without any additional information,
-    2. Question that requires context from the O2 simulation documentation.
-    
+    content="""You are a question classifier.
+    Your task is to classify the following question into one of the categories:
+    1. General conversation question - can be answered without any additional context,
+    2. Expert question about O2 simulations - requires context from the internal documentation,
     Respond only with the category number (1 or 2) and nothing else."""
 )
 
 basic_response_system_message = SystemMessage(
-    content="""You are a helpful assistant."""
+    content="""You are a helpful and kind assistant."""
 )
 
 rag_response_system_message = SystemMessage(
-    content="""You are a chatbot designed to help with the 02 simulation documentation.
-    Use the provided context to answer the following question.
-    If the context does not contain enough (or any) relevant information say that you do not know the answer.
-    Also cite your sources like [n] and do not make up new information."""
+    content="""You are a chatbot designed to assist users with O2 simulation documentation.
+    Use the provided context to answer the following question:
+    - If the context contains relevant information, use it to provide a clear answer.
+    - If the context does not contain enough (or any) relevant information, say that you do not know the answer.
+    - Do not explain the context, just use it to directly answer the question.
+    - Do not make up new information.
+    - If there are any sources related to your answer, cite them like [n]."""
 )
